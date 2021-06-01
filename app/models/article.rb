faf0fc42b098fb08fcbc3a5da22c74f44227df73
Vincenzo Galante <mr.vincenzo.galante@gmail.com>
2021-06-01 11:50:18 +0200

    added validations for each model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   has_many :folders, through: :connections
+  validates :url, presence: true
+  validates :title, presence: true
+  validates :status, presence: true
 end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,5 @@
 class Folder < ApplicationRecord
   belongs_to :user
   has_many :articles, through: :connections
+  validates :title, presence: true
 end
