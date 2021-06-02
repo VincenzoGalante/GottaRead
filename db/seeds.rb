5ec78eac0c06f30acebccf6b0064a9b98275fb28
Vincenzo Galante <mr.vincenzo.galante@gmail.com>
2021-06-02 12:44:39 +0200

    Solved DB issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,50 @@ User.destroy_all
 
 puts "Destroyed sample data 🤯"
 
+
+#Creating Users
+  User.create!(
+    email: "gigi@socklove.com",
+    first_name: "Gigi",
+    last_name: "Readalot",
+    password: "123456"
+    )
+
+puts "Created user 👤"
+
+
+#Creating Folders
+  Folder.create!(
+    title: "Freediving",
+    description: "Underwater folder, blub, blub",
+    image: "freediving_folder",
+    color: "black",
+    user_id: User.first.id
+    )
+
+  Folder.create!(
+    title: "Tennis",
+    description: "Folder for my tennis related research",
+    image: "tennis_folder",
+    color: "yellow",
+    user_id: User.first.id
+    )
+
+  Folder.create!(
+    title: "Neuroscience",
+    description: "For university and PHD research",
+    image: "neuroscience_folder",
+    color: "red",
+    user_id: User.first.id
+    )
+
+puts "Created folder 🗂"
+
+
 #Creating Articles
   #DIVING
     #1
-    Article.create(
-      id: 1,
+    Article.create!(
       url: "https://www.reddit.com/r/learnprogramming/comments/nkbkf2/from_zero_coding_experience_at_the_age_of_24/",
       title: "Reddit, LearnProgramming, From Zero to Hero",
       note: "Story of a self-thought Pythong developer",
@@ -27,8 +66,7 @@ puts "Destroyed sample data 🤯"
       )
 
     #2
-    Article.create(
-      id: 2,
+    Article.create!(
       url: "https://gofreediving.co.uk/definitive-guide-frenzel-technique",
       title: "The Definitive Guide to the Frenzel Technique – With Emma Farrell",
       note: "Freediving, Frenzel, Diving techniques",
@@ -38,8 +76,7 @@ puts "Destroyed sample data 🤯"
     )
 
     #3
-    Article.create(
-      id: 3,
+    Article.create!(
       url: "https://gofreediving.co.uk/day-13-volunteering-at-turtle-rescue-sanctuary-the-olive-ridley-project",
       title: "Day 13: Volunteering at Turtle Rescue Sanctuary – The Olive Ridley Project",
       note: "Turlte Sanctuary, to read for next vacation",
@@ -49,8 +86,7 @@ puts "Destroyed sample data 🤯"
     )
 
     #4
-    Article.create(
-      id: 4,
+    Article.create!(
       url: "https://www.bbc.com/news/world-europe-33789773",
       title: "Freediving: The lure of the deep",
       note: "",
@@ -61,7 +97,6 @@ puts "Destroyed sample data 🤯"
 
     #5
     Article.create(
-      id: 5,
       url: "https://theconversation.com/free-divers-have-long-defied-science-and-we-still-dont-really-understand-how-they-go-so-deep-92690",
       title: "Free divers have long defied science – and we still don’t really understand how they go so deep",
       note: "Science related freediving article, weekend read",
@@ -73,7 +108,6 @@ puts "Destroyed sample data 🤯"
   #TENNIS
     #6
     Article.create(
-      id: 6,
       url: "https://www.nytimes.com/2021/05/31/sports/tennis/2021-french-open-what-to-watch-tuesday.html",
       title: "2021 French Open: What to Watch on Tuesday",
       note: "French Open, Sunday read",
@@ -84,7 +118,6 @@ puts "Destroyed sample data 🤯"
 
     #7
     Article.create(
-      id: 7,
       url: "https://www.jhse.ua.es/article/view/2020-v15-n1-ball-change-tennis-match-characteristics-rally-pace-grand-slam-tournaments",
       title: "Ball change in tennis: How does it affect match characteristics and rally pace in Grand Slam tournaments?",
       note: "Paper on tennis balls",
@@ -95,7 +128,6 @@ puts "Destroyed sample data 🤯"
 
     #8
     Article.create(
-      id: 8,
       url: "https://www.tennismindgame.com/serena-williams-tennis-lessons.html",
       title: "Serena Williams Masterclass Video Course Review",
       note: "Masterclass review",
@@ -106,7 +138,6 @@ puts "Destroyed sample data 🤯"
 
     #9
     Article.create(
-      id: 9,
       url: "https://www.bloomberg.com/news/features/2021-03-24/novak-djokovic-other-tennis-players-seek-to-reform-economics-of-the-sport",
       title: "The Missed Business Opportunity That Is Pro Tennis",
       note: "Weekend read on tennis as a business",
@@ -117,7 +148,6 @@ puts "Destroyed sample data 🤯"
 
     #10
     Article.create(
-      id: 10,
       url: "https://bleacherreport.com/articles/1857664-why-is-american-tennis-dying",
       title: "Why Is American Tennis Dying?",
       note: "Article on declining importance of tennis in US",
@@ -129,7 +159,6 @@ puts "Destroyed sample data 🤯"
   #NEUROSCIENE
     #11
     Article.create(
-      id: 11,
       url: "https://www.nature.com/articles/d41586-021-01353-9",
       title: "Activation of retinal neurons triggers tumour formation in cancer-prone mice",
       note: "Weekend read about tumour formation",
@@ -140,7 +169,6 @@ puts "Destroyed sample data 🤯"
 
     #12
     Article.create(
-      id: 12,
       url: "https://www.nature.com/articles/s41591-021-01385-8",
       title: "Putting the MD back into MDMA",
       note: "Article on MDMA for PTSD",
@@ -151,7 +179,6 @@ puts "Destroyed sample data 🤯"
 
     #13
     Article.create(
-      id: 13,
       url: "https://www.jneurosci.org/spotlight",
       title: "Annual Report on JNeurosci Spotlight",
       note: "Long weekend read about NS news",
@@ -162,7 +189,6 @@ puts "Destroyed sample data 🤯"
 
     #14
     Article.create(
-      id: 14,
       url: "https://neurosciencenews.com/omega-3-psychosis-18527/",
       title: "Low Levels of Omega-3 Associated With Higher Risk of Psychosis",
       note: "Omega 3 and psychosis correlation",
@@ -173,7 +199,6 @@ puts "Destroyed sample data 🤯"
 
     #15
     Article.create(
-      id: 15,
       url: "https://www.the-scientist.com/reading-frames/opinion-the-overlooked-power-of-inhibitory-neurons-68819",
       title: "Opinion: The Overlooked Power of Inhibitory Neurons",
       note: "Short article, quick read",
@@ -184,121 +209,91 @@ puts "Destroyed sample data 🤯"
 
 puts "Created articles 🐙🎾🧠"
 
-#Creating Folders
-  Folder.create(
-    id: 1,
-    title: "Freediving",
-    description: "Underwater folder, blub, blub",
-    image: "freediving_folder",
-    color: "black",
-    user_id: 1
-    )
-
-  Folder.create(
-    id: 2,
-    title: "Tennis",
-    description: "Folder for my tennis related research",
-    image: "tennis_folder",
-    color: "yellow",
-    user_id: 1
-    )
-
-  Folder.create(
-    id: 3,
-    title: "Neuroscience",
-    description: "For university and PHD research",
-    image: "neuroscience_folder",
-    color: "red",
-    user_id: 1
-    )
-
-puts "Created folder 🗂"
-
-#Creating Users
-  User.create(
-    id: 1,
-    email: "gigi@socklove.com",
-    first_name: "Gigi",
-    last_name: "Readalot",
-    password: "123456"
-    )
-
-puts "Created user 👤"
 
 #Creating Connections
-  Connection.new(
-    folder_id: 1,
-    article_id: 1,
+
+  #ITERATION REFACTORING
+  #iterate over my folders
+  #for each folder n = index + 1
+  #all articles, from n until n+4
+
+  first_folder = Folder.first
+
+  first_folder.articles << Article.first
+
+  # Connection.new(
+  #   folder_id: Folder.first.id,
+  #   article_id: Article.first.id,
+  #   )
+
+  Connection.create!(
+    folder_id: Folder.first.id,
+    article_id: Article.first.id + 1,
     )
 
-  Connection.new(
-    folder_id: 1,
-    article_id: 2,
+  Connection.create!(
+    folder_id: Folder.first.id,
+    article_id: "#{Article.first.id + 2}",
     )
 
-  Connection.new(
-    folder_id: 1,
-    article_id: 3,
+  Connection.create!(
+    folder_id: Folder.first.id,
+    article_id: "#{Article.first.id + 3}",
     )
 
-  Connection.new(
-    folder_id: 1,
-    article_id: 4,
+  Connection.create!(
+    folder_id: Folder.first.id,
+    article_id: "#{Article.first.id + 4}",
     )
 
-  Connection.new(
-    folder_id: 1,
-    article_id: 5,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 1}",
+    article_id: "#{Article.first.id + 5}",
     )
 
-  Connection.new(
-    folder_id: 2,
-    article_id: 6,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 1}",
+    article_id: "#{Article.first.id + 6}",
     )
 
-  Connection.new(
-    folder_id: 2,
-    article_id: 7,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 1}",
+    article_id: "#{Article.first.id + 7}",
     )
 
-  Connection.new(
-    folder_id: 2,
-    article_id: 8,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 1}",
+    article_id: "#{Article.first.id + 8}",
     )
 
-  Connection.new(
-    folder_id: 2,
-    article_id: 9,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 1}",
+    article_id: "#{Article.first.id + 9}",
     )
 
-  Connection.new(
-    folder_id: 2,
-    article_id: 10,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 2}",
+    article_id: "#{Article.first.id + 10}",
     )
 
-  Connection.new(
-    folder_id: 3,
-    article_id: 11,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 2}",
+    article_id: "#{Article.first.id + 11}",
     )
 
-  Connection.new(
-    folder_id: 3,
-    article_id: 12,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 2}",
+    article_id: "#{Article.first.id + 12}",
     )
 
-  Connection.new(
-    folder_id: 3,
-    article_id: 13,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 2}",
+    article_id: "#{Article.first.id + 13}",
     )
 
-  Connection.new(
-    folder_id: 3,
-    article_id: 14,
-    )
-
-  Connection.new(
-    folder_id: 3,
-    article_id: 15,
+  Connection.create!(
+    folder_id: "#{Folder.first.id + 2}",
+    article_id: "#{Article.first.id + 14}",
     )
 
 puts "Created connections 🤜🏻🤛🏿"
