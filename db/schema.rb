fbe267b1b203eba6f62ab7e925ab7551fe5c0e0f
Vincenzo Galante <mr.vincenzo.galante@gmail.com>
2021-06-03 12:30:29 +0200

    User reference to Article + Folder creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_140422) do
+ActiveRecord::Schema.define(version: 2021_06_03_102715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2021_06_01_140422) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "connections", force: :cascade do |t|
@@ -67,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_140422) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "connections", "articles"
   add_foreign_key "connections", "folders"
   add_foreign_key "folders", "users"
