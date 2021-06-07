7e457d3cad6f34ffcf4e1586b5ddac09973c4771
Vincenzo Galante <mr.vincenzo.galante@gmail.com>
2021-06-07 12:51:40 +0200

    card design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def dashboard
+    @folders = Folder.all
+    @freediving_articles = Article.joins(:folders).where(folders:{title:"Freediving"})
+    @neuroscience_articles = Article.joins(:folders).where(folders:{title:"Neuroscience"})
+    @tennis_articles = Article.joins(:folders).where(folders:{title:"Tennis"})
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def Status(article.status)
+    if article.status == "to_read"
+      puts "<%= link_to "To read", mark_as_done_path(article) %>"
+    elsif article.status == "missed"
+      puts "<%= link_to "Missed", mark_as_done_path(article) %>"
+    elsif article.status == "done"
+      puts "<%= link_to "Done", mark_as_done_path(article) %>"
+    end
+  end
 end
